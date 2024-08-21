aeb4fa7656ce17fe521eaadcfc20b3c09a0005d5
AliciaSilva89 <142455850+AliciaSilva89@users.noreply.github.com>
2024-08-21 12:19:21 -0300

    Add files via upload
    Add changes class books

--- src/Books.java
+++ src/Books.java
@@ -2,44 +2,37 @@ public class Books extends BibliographicItem {
     private String author;
     private String isbn;
 
-    public Books(String author, String isbn,String title, String code, int yearOfPublication){
+    public Books(String author, String isbn, String title, String code, int yearOfPublication) {
         super(title, code, yearOfPublication);
         this.author = author;
         this.isbn = isbn;
     }
 
-    public void Validate_fields() {
-        this.author = author;
-        this.isbn = isbn;
+    // Valida campos
+    public boolean validateFields() {
+        return author != null && !author.isEmpty() && isbn != null && (isbn.length() >= 10 && isbn.length() <= 13);
     }
 
-    //Author
-    public String getAuthor() {//get
+    //Autor
+    public String getAuthor() {// Get
         return author;
     }
 
-
-    //Isbn
+    //ISBN
     public String getIsbn() {//get
         return isbn;
     }
 
     @Override
     public String toString() {
-        return "Books:" +getTitle() + ", Author: " + author + ", ISBN: " + isbn + "Year:" + getYearOfPublication() + ", Title: " + getTitle() + ", Code: " + getCode();
+        return "Books: " + getTitle() + ", Author: " + author + ", ISBN: " + isbn + ", Year: " + getYearOfPublication() + ", Code: " + getCode();
     }
-    //Validando Autor e ISBN
-    public Boolean ValidateItemBooks(String author, String isbn) {
-        if(!ValidateBibliographicItem()){
+
+    // Validando Autor e ISBN
+    public boolean validateItemBooks() {
+        if (!validateFields()) {
             return false;
         }
-
-        if(author != null) {
-            return false;
-        }
-
-        if(isbn != null || isbn.length() < 10 && isbn.length() > 13) {}
-        return false;
-    }
         return true;
+    }
 }
